0d2e2023bf1748c8afcaf06788aa1447d7515cd7
daviscvance <daviscvance@gmail.com>
2023-09-05 23:10:44 -0400

    2023-09-05 revision

--- sliding_window/3_longest_substring_without_repeating_characters.py
+++ sliding_window/3_longest_substring_without_repeating_characters.py
@@ -4,26 +4,46 @@
 #
 # Find the longest substring length without repeating characters.
 
-# O(N^2) time complexity (worst case, even though it doesnt)
+# O(N) time complexity, O(m) space complexity
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        longest = 1
-        S = len(s)
-        if not S:
-            return S
-        for l in range(S):
-            r = 1
-            # As long as we dont look beyond the end of str
-            # and the current letter is not in the past substr
-            # keep looking for a substr longer than our longest
-            while l + longest < S and s[l+r] not in s[l:l+r]:
-                if len(s[l:l+r+1]) <= longest:
-                    r += 1
-                    continue
-                longest = max(longest, len(s[l:l+r+1]))
-                r += 1
-                if l + longest > S:
-                    return longest
-            continue
+        l, longest, seen = 0, 0, {}
+        for r in range(len(s)):
+            # If the current letter has not been seen at all
+            #  or the occurrences is less than the left side of window
+            #  meaning it has fallen out of the sliding window,
+            # Then the longest chain can be updated perhaps with length
+            #  of the current window.
+            if s[r] not in seen or seen[s[r]] < l:
+                longest = max(longest, r - l + 1)
+            # If not, then we are seeing a duplicate and need to move
+            # the left side of the window past the right side of the
+            # window.
+            else:
+                l = seen[s[r]] + 1
+            # and now we have seen the last character r times.
+            seen[s[r]] = r
         return longest
+
+# # # O(N^2) time complexity (worst case, even though it doesnt)
+# # class Solution:
+# #     def lengthOfLongestSubstring(self, s: str) -> int:
+# #         S, longest = len(s), 1
+# #         if not S:
+# #             return S
+# #         for l in range(S):
+# #             r = 1
+# #             # As long as we dont look beyond the end of str
+# #             # and the current letter is not in the past substr
+# #             # keep looking for a substr longer than our longest
+# #             while l + longest < S and s[l+r] not in s[l:l+r]:
+# #                 if len(s[l:l+r+1]) <= longest:
+# #                     r += 1
+# #                     continue
+# #                 longest = max(longest, len(s[l:l+r+1]))
+# #                 r += 1
+# #                 if l + longest > S:
+# #                     return longest
+# #             continue
+# #         return longest
 
