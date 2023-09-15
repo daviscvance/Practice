79822b5ec1ab73f94c5b9b6cd431dd1e3b39bb5d
daviscvance <daviscvance@gmail.com>
2023-09-14 23:44:11 -0400

    2023-09-14

---
+++ sliding_window/easy/219-contains-duplicate-ii.py
@@ -0,0 +1,17 @@
+# 219. Contains Duplicate II
+# Easy
+# Array, Hash Table, Sliding Window
+# https://leetcode.com/problems/contains-duplicate-ii
+#
+# Find if there are two indices i and j in an array that are equal and less than k units apart.
+
+class Solution:
+    # Index Memo: Time: O(N) | Space: O(k)
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
+        hash_map = {}
+        for idx, num in enumerate(nums):
+            if num in hash_map and abs(hash_map[num] - idx) <= k:
+                return True
+            hash_map[num] = idx
+        else:
+            return False
