66b6041b5e6dcce6374acbb26b79c5ee454eb21c
daviscvance <daviscvance@gmail.com>
2023-09-18 23:30:42 -0400

    resave 383

---
+++ string/easy/383-ransom-note.py
@@ -0,0 +1,25 @@
+# 383. Ransom Note
+# Easy
+# Hash Table, String, Counting
+# https://leetcode.com/problems/ransom-note/
+#
+# Determine if a ransomNote can be constructed by using the letters (once) from magazine.
+
+from collections import Counter
+
+class Solution:
+    # Hash Table | Time: O(N+M) | Space: O(n+m)
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        note, mgz = Counter(ransomNote), Counter(magazine)
+        # Intersection of note and mgz Counter objects is equal to note Counter object determines
+        # all note letters are available in magazine.
+        return note & mgz == note
+
+# class Solution:
+#     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+#         letters = Counter(magazine)
+#         for letter in ransomNote:
+#             if letter not in letters or letters[letter] == 0:
+#                 return False
+#             letters[letter] -= 1
+#         return True
