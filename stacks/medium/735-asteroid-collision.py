df3cbcfa62685305801126b4489a06709907409e
daviscvance <daviscvance@gmail.com>
2023-09-26 23:27:11 -0400

    2023-09-26

---
+++ stacks/medium/735-asteroid-collision.py
@@ -0,0 +1,25 @@
+# 735. Asteroid Collision
+# Medium
+# Array, Stack, Simulation
+# https://leetcode.com/problems/asteroid-collision
+#
+# Compare asteroid sizes and directions (pos→ neg←) and collect remaining asteroids after collisions.
+# def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+
+from collections import deque
+
+class Solution:
+    # Time: O(n) | Space: O(n)
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = deque()
+        for a in asteroids:
+            while stack and a < 0 < stack[-1]:
+                if stack[-1] < abs(a):
+                    stack.pop()
+                    continue  # Next asteroid.
+                elif stack[-1] == abs(a):
+                    stack.pop()
+                break  # Asteroid must be destroyed (do not append below).
+            else:
+                stack.append(a)
+        return stack
