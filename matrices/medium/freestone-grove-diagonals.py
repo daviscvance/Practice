eeda5258abfff0d1484b36320e71dc92e7036a6f
daviscvance <daviscvance@gmail.com>
2023-10-10 23:53:41 -0400

    update diagonals with other directions

--- matrices/medium/freestone-grove-diagonals.py
+++ matrices/medium/freestone-grove-diagonals.py
@@ -4,21 +4,76 @@
 # Freestone Grove Partners
 
 class Solution:
-    def up_right_anti_diagonal(self, matrix: list[list[int]]) -> None: 
-        diags, pad = [], []
+
+    def up_right_diagonal_edges(self, matrix: list[list[int]]) -> None: 
+        diagonals, pad = [], []
 
         while any(matrix):
             edge = [*next(zip(*matrix)), *matrix[-1][1:]]
             matrix = [row[1:] for row in matrix[:-1]]
-            diags.append(pad+edge+pad)
+            diagonals.append(pad + edge + pad)
             pad.append(0)
-        return [*map(list,zip(*diags))]
+        return [*map(list, zip(*diagonals))]
+    
+    # 1 2 3  1 2 3
+    # 4 5 6  x x 6
+    # 7 8 9  x x 9  ==> 1 2 3 6 9  (pad = none)
+    #                   | | | | |
+    # 4 5    4 5        | | | | |
+    # 7 8    x 8    ==> 0 4 5 8 0  (pad = 0)
+    #                   | | | | |
+    # 7      7      ==> 0 0 7 0 0  (pad = 0,0)
+    #                   | | | | |
+    #                   \ \ \ \ \__ [9,0,0]     zipped
+    #                    \ \ \ \___ [6,8,0]
+    #                     \ \ \____ [3,5,7]
+    #                      \ \_____ [2,4,0]
+    #                       \______ [1,0,0]
+
+    def down_left_diagonal_edges(self, matrix: list[list[int]]) -> None: 
+        diagonals, pad = [], []
+
+        while any(matrix):
+            edge = [*matrix[0][:-1], *next(zip(*map(reversed, matrix)))]
+            matrix = [row[:-1] for row in matrix[1:]]
+            diagonals.append(pad + edge + pad)
+            pad.append(0)        
+        return [*map(list, zip(*diagonals))]
+    
+    def down_right_diagonal_edges(self, matrix: list[list[int]]) -> None: 
+        diagonals, pad = [], []
+        while any(matrix):
+            edge = [*next(zip(*reversed(matrix))), *matrix[0][1:]]
+            matrix    = [row[1:] for row in matrix[1:]]
+            diagonals.append(pad + edge + pad)
+            pad.append(0)        
+        return [*map(list, zip(*diagonals))]
+    
+    def up_right_diagonal_simple(self, matrix: list[list[int]]) -> None:
+        rows, cols = len(matrix), len(matrix[0])
+        diagonals = [[] for i in range(rows + cols - 1)] 
+        # for row in range(rows): 
+        #     for col in range(cols): 
+        #         diagonals[row + col].append(matrix[col][row]) 
+        
+        # for row in range(len(diagonals)): 
+        #     for col in range(len(diagonals[row])): 
+        #         print(diagonals[row][col], end = " ") 
+        #     print()
 
 matrix = [
     [1,  2,  3,  4],
     [5,  6,  7,  8], 
     [9, 10, 11, 12]]
 
-for i in Solution().up_right_anti_diagonal(matrix):
+for i in Solution().up_right_diagonal_edges(matrix):
     [print(x, end=' ') for x in i if x]
     print()
+for i in Solution().down_right_diagonal_edges(matrix):
+    [print(x, end=' ') for x in i if x]
+    print()
+for i in Solution().down_left_diagonal_edges(matrix):
+    [print(x, end=' ') for x in i if x]
+    print()
+
+
