ec1bf03d8e881deb198c8f742b97bac9297d4f92
daviscvance <daviscvance@gmail.com>
2023-10-14 23:50:45 -0400

    2023-10-14

---
+++ heaps/easy/1046-last-stone-weight.py
@@ -0,0 +1,23 @@
+# 1046. Last Stone Weight
+# Easy
+# Array, Heap (Priority Queue)
+# https://leetcode.com/problems/last-stone-weight
+#
+# For an array of weights, smash the heaviest 2 stones negating the weight difference until there
+# there is one or none left. Return the last value.
+# def lastStoneWeight(self, stones: List[int]) -> int:
+# Input: stones = [2,7,4,1,8,1]
+# Output: 1
+
+from heapq import heapify, heappop, heappush
+from operator import neg
+
+class Solution:
+    # Max Heap | Time: O(n + k log n) | Space: O(n)
+    def lastStoneWeight(self, stones: list[int]) -> int:
+        stones = list(map(neg, stones))  # Invert minHeap to maxHeap.
+        heapify(stones)
+        while len(stones) > 1:  # Keep smashing 2 largest stones together.
+            if (largest := heappop(stones)) != (largest_2nd := heappop(stones)):
+                heappush(stones, largest - largest_2nd)  # Add the weight difference back.
+        return abs(stones[0]) if stones else 0
---
+++ heaps/easy/703-kth-largest-element-in-a-stream.py
@@ -0,0 +1,31 @@
+# 703. Kth Largest Element in a Stream
+# Easy
+# Binary Tree, Design, Binary Search Tree, Heap (Priority Queue), Data Stream
+# https://leetcode.com/problems/kth-largest-element-in-a-stream
+#
+# Design a class to find the kth largest element in a stream. 
+# class KthLargest:
+#     def __init__(self, k: int, nums: List[int]):
+#     def add(self, val: int) -> int:
+# Input
+# ["KthLargest", "add", "add", "add", "add", "add"]
+# [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+# Output
+# [null, 4, 5, 5, 8, 8]
+
+import heapq
+
+class KthLargest:
+    # MinHeap | Time: O(k) | Space: O(n)
+    def __init__(self, k: int, nums: list[int]):
+        # Intialize with only top k elements, then convert to heap.
+        self.heap = heapq.nlargest(k, nums)
+        heapq.heapify(self.heap)
+        self.k = k
+
+    def add(self, val: int) -> int:
+        if len(self.heap) < self.k:  # Add value if theres space.
+            heapq.heappush(self.heap, val)
+        elif val > self.heap[0]:  # Keep k len with the minimum.
+            heapq.heapreplace(self.heap, val)
+        return self.heap[0]  # Return minimum.
