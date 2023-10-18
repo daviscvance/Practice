2f042a145b58316aa0ded16b674b0c7212e10d8b
daviscvance <daviscvance@gmail.com>
2023-10-18 00:01:29 -0400

    2023-10-17 late submission

---
+++ heaps/medium/1481-least-number-of-unique-integers-after-k-removals.py
@@ -0,0 +1,20 @@
+# 1481. Least Number of Unique Integers after K Removals
+# Medium
+# Array, Hash Table, Greedy, Sorting, Counting
+# https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals
+#
+# Find the least number of unique integers after removing k elements.
+# def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+# Input: arr = [4,3,1,1,3,3,2], k = 3
+# Output: 2
+
+from collections import Counter
+from heapq import heapify, heappop
+
+class Solution:
+    # Min Heap + Counter | Time: O(n + k log n) | Space: O(n)
+    def findLeastNumOfUniqueInts(self, arr: list[int], k: int) -> int:
+        heapify(min_heap := list(Counter(arr).values()))
+        while k > 0:
+            k -= heappop(min_heap)
+        return len(min_heap) + (k < 0)
