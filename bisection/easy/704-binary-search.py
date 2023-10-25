31f05ae6344ea398f9035177d3ac6c68688e92f9
daviscvance <daviscvance@gmail.com>
2023-10-24 23:43:21 -0400

    2023-10-23

---
+++ bisection/easy/704-binary-search.py
@@ -0,0 +1,22 @@
+# 704. Binary Search
+# Easy
+# Array, Binary Search
+# https://leetcode.com/problems/binary-search
+#
+# Write a classic binary search algorithm.
+# def search(self, nums: List[int], target: int) -> int:
+# Input: nums = [-1,0,3,5,9,12], target = 9
+# Output: 4
+
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        lo, hi = 0, len(nums) - 1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return -1
