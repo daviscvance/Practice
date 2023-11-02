208726739eb47c72f83095588659f8254b98d72d
daviscvance <daviscvance@gmail.com>
2023-11-01 23:53:58 -0400

    2023-10-31

--- Leetcode/Python/largest-number-less-than-n.py
+++ Leetcode/Python/largest-number-less-than-n.py
@@ -19,12 +19,21 @@ def largest_number_less_than_n(n: int, A):
 
     return int(''.join(map(str, helper(0, False))))
 
+
+# This function works by iterating over the digits of n from left to right. For each digit, it
+# tries to fit the largest possible number from A. If it can’t fit any number from A that is less
+# than or equal to the current digit of n, it backtracks and continues with the next largest number
+# from A. The flag variable is used to track whether we have already placed a digit that is less
+# than the corresponding digit in n, which allows us to place any digit from A in the remaining
+# positions. The function returns the largest number composed of elements in A that is less than n.
+# If no such number exists, it returns None.
+
 # Test the function
 ins = [23121, 24121, 29513]
-outs = [22999, 22999, 39499]
+outs = [22999, 22999, 29499]
 A = {2, 4, 9}
 
 for input, output in list(zip(ins, outs)):
-    print(f'Testing: {input=}')
+    print(f'Testing: {input=} -> {output=} ', end='')
     assert(largest_number_less_than_n(n = input, A=A) == output)
-    print(f'Passed: {input=} -> {output=}')
+    print(f'... Passed!')
