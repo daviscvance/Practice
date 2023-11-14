be4827b81dc8a58daa60e0591dd7e5ea5acaf7c8
daviscvance <daviscvance@gmail.com>
2023-11-13 23:54:11 -0500

    2023-11-13

---
+++ Leetcode/Python/linked_lists/easy/206-reverse-linked-list.py
@@ -0,0 +1,28 @@
+# 206. Reverse Linked List
+# Easy
+# Linked List, Recursion
+# https://leetcode.com/problems/reverse-linked-list
+#
+# Reverse a singly linked list.
+# def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+# Input: head = [1,2,3,4,5]
+# Output: [5,4,3,2,1]
+
+from typing import Optional
+
+class ListNode:
+    # Definition for singly-linked list.
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    # Iterative | Two Pointer | Time: O(n) | Space: O(1)
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        past, present = None, head
+        while present:
+            future = present.next  # Save the next node before breaking the chain.
+            present.next = past  # Reverse the link direction.
+            past = present  # Move first pointer down towards the end.
+            present = future  # Move second pointer 1 further from the new past.
+        return past  # New head of reversed singly-linked list.
---
+++ Leetcode/Python/linked_lists/easy/21-merge-two-sorted-lists.py
@@ -0,0 +1,37 @@
+# 21. Merge Two Sorted Lists
+# Easy
+# Linked List, Recursion
+# https://leetcode.com/problems/merge-two-sorted-lists
+#
+# Merge two sorted linked lists and return the head of the sorted linked list.
+# def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+# Input: list1 = [1,2,4], list2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+
+from typing import Optional
+
+class ListNode:
+    # Definition for singly-linked list.
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    # Iterative | Time: O(n+m) | Space: O(n+m)
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        merged = prev = ListNode()
+        def compare(x: int, y: int) -> ListNode:
+            if x.val < y.val:
+                return x
+            else:
+                return y
+
+        while l1 and l2:
+            prev.next = prev = (mini := compare(l1, l2))
+            if mini == l1:
+                l1 = l1.next
+            else:
+                l2 = l2.next
+
+        prev.next = l1 or l2
+        return merged.next
