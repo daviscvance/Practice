8bee01d49e6eb33db08ddeb58c318ab75455654b
daviscvance <daviscvance@gmail.com>
2023-11-15 23:56:47 -0500

    2023-11-15

---
+++ Leetcode/Python/linked_lists/easy/141-linked-list-cycle.py
@@ -0,0 +1,38 @@
+# 141. Linked List Cycle
+# Easy
+# Hash Table, Linked List, Two Pointers
+# https://leetcode.com/problems/linked-list-cycle
+#
+# Determine if a linked list has a cycle (looped node reference).
+# def hasCycle(self, head: Optional[ListNode]) -> bool:
+# Input: head = [3,2,0,-4], pos = 1
+# Output: true (b/c pos >= 0)
+
+from typing import Optional
+
+class ListNode:
+    # Definition for singly-linked list.
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    # (2) Visited Set | Time: O(n) | Space: O(n)
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = set()
+        while head:
+            if head in visited:
+                return True
+            else:
+                visited.add(head)
+                head = head.next
+        return False
+
+    # (1) Speed trap / Tortoise & Hare / Floyd's Cycle Finder | Time: O(n) | Space: O(1)
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast:
+                return True
+        return False
---
+++ Leetcode/Python/linked_lists/easy/234-palindrome-linked-list.py
@@ -0,0 +1,51 @@
+# 234. Palindrome Linked List
+# Easy
+# Linked List, Two Pointers, Stack, Recursion
+# https://leetcode.com/problems/palindrome-linked-list
+#
+# Determine if the linked list is a palindrome.
+# def isPalindrome(self, head: Optional[ListNode]) -> bool:
+# Input: head = [1,2,2,1]
+# Output: true
+
+from typing import Optional
+
+class ListNode:
+    # Definition for singly-linked list.
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    # (2) Stack | Time: O(n) | Space: O(n)
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        stack = [head.val]
+        curr = head
+        while curr:
+            stack.append(curr.val)
+            curr = curr.next
+        curr = head
+        while curr:
+            if stack.pop() != curr.val:
+                return False
+            curr = curr.next
+        return True
+            
+    # (1) Two Pointer | Time: O(n) | Space: O(1)
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+
+        prev = None
+        while slow:
+            curr = slow
+            slow = slow.next
+            curr.next = prev
+            prev = curr
+        while prev:
+            if prev.val != head.val:
+                return False
+            prev = prev.next
+            head = head.next
+        return True
