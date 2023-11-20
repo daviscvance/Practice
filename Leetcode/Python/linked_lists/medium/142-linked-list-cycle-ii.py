4b174742a0d217f6cb4d52c678ebdca48b97ca51
daviscvance <daviscvance@gmail.com>
2023-11-19 22:42:12 -0500

    2023-11-19

---
+++ Leetcode/Python/linked_lists/medium/142-linked-list-cycle-ii.py
@@ -0,0 +1,41 @@
+# 142. Linked List Cycle II
+# Medium
+# Hash Table, Linked List, Two Pointers
+# https://leetcode.com/problems/linked-list-cycle-ii
+#
+# Find the node where a cycle begins, if it exists.
+# def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+# Input: head = [3,2,0,-4], pos = 1
+# Output: index 1
+
+from typing import Optional
+
+class ListNode:
+    # Definition for singly-linked list.
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    # Hash Set | Time: O(n) | Space: O(n)
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        nodes = set()
+        while head and head.next:
+            if head in nodes:
+                return head
+            else:
+                nodes.add(head)
+            head = head.next
+        return None
+
+    # Two Pointer / Floyd's Cycle Detector | Time: O(n) | Space: O(1)
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast:  # Cycle detected.
+                slow = head  # Reset slow pointer.
+                while slow != fast:  # Move in unison until they match.
+                    slow, fast = slow.next, fast.next
+                return slow  # Match is at the beginning of cycle.
+        return None
---
+++ Leetcode/Python/linked_lists/medium/19-remove-nth-node-from-end-of-list.py
@@ -0,0 +1,34 @@
+# 19. Remove Nth Node From End of List
+# Medium
+# Linked List, Two Pointers
+# https://leetcode.com/problems/remove-nth-node-from-end-of-list
+#
+# Remove the nth node from the end of a linked list
+# def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+# Input: head = [1,2,3,4,5], n = 2
+# Output: [1,2,3,5]
+
+from typing import Optional
+
+class ListNode:
+    # Definition for singly-linked list.
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    # Two Pointer | Time: O(n) | Space: O(1)
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # Run two pointers to the end such that previous is 1 behind the target.
+        curr = prev = head
+        [(curr := curr.next) for _ in range(n)]  # curr is n nodes ahead of prev.
+        if not curr:  # n is first node.
+            return head.next
+        while curr and curr.next:  # prev.next lands at target node.
+            prev, curr = prev.next, curr.next
+
+        # Delete the target node.
+        prev.next = prev.next.next
+        return head
+
+        
