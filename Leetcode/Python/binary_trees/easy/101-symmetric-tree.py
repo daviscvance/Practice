3788bf4339e538b6a7e257b9108047019702cabc
daviscvance <daviscvance@gmail.com>
2023-11-28 21:19:42 -0500

    2023-11-27

---
+++ Leetcode/Python/binary_trees/easy/101-symmetric-tree.py
@@ -0,0 +1,33 @@
+# 101. Symmetric Tree
+# Easy
+# Tree, Depth-First Search, Breadth-First Search, Binary Tree
+# https://leetcode.com/problems/symmetric-tree
+#
+# Check if a binary tree has mirrored symmetry around its center.
+# def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+# Input: root = [1,2,2,null,3,null,3]
+# Output: false
+
+from typing import Optional
+
+class TreeNode:
+    # Definition for a binary tree node.
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    # Recursion | Time: O(n) | Space: O(n)
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def isMirror(left, right):
+            if left and right:
+                return (
+                    left.val == right.val
+                    and isMirror(left.left, right.right)  # Outer.
+                    and isMirror(left.right, right.left)  # Inner.
+                )    
+            else:  # Either left or right is None (or both are).
+                return left is right
+
+        return isMirror(root.left, root.right)
---
+++ Leetcode/Python/binary_trees/easy/104-maximum-depth-of-binary-tree.py
@@ -0,0 +1,28 @@
+# 104. Maximum Depth of Binary Tree
+# Easy
+# Tree, Depth-First Search, Breadth-First Search, Binary Tree
+# https://leetcode.com/problems/maximum-depth-of-binary-tree
+#
+# Return the maximum depth of a binary tree.
+# def maxDepth(self, root: Optional[TreeNode]) -> int:
+# Input: root = [3,9,20,null,null,15,7]
+# Output: 3
+
+from typing import Optional
+
+class TreeNode:
+    # Definition for a binary tree node.
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    # DFS | Time: O(n) | Space: O(h)
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(root: TreeNode, depth: int) -> int:
+            if not root:
+                return depth
+            return max(dfs(root.left, depth + 1), dfs(root.right, depth + 1))
+
+        return dfs(root, 0)
