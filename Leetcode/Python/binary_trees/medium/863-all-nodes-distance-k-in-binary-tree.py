b36055b361ba6de8ca5c75601618e7a90a2a3315
daviscvance <daviscvance@gmail.com>
2024-01-11 23:59:32 -0500

    2024-01-11

--- Leetcode/Python/binary_trees/medium/863-all-nodes-distance-k-in-binary-tree.py
+++ Leetcode/Python/binary_trees/medium/863-all-nodes-distance-k-in-binary-tree.py
@@ -8,7 +8,7 @@
 # Input: root = [3,5,1,6,2,0,8,null,null,7,4], target = 5, k = 2
 # Output: [7,4,1]
 
-from typing import List
+from typing import List, Optional
 
 class TreeNode:
     # Definition for a binary tree node.
@@ -20,15 +20,16 @@ class TreeNode:
 class Solution:
     # Recursive Pre-Order Traversal | Time: O(n) | Space: O(k log n)
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        # dfs
-        # search for target (node.val = target)
-        # if not target val:
-        #       move on with life
-        #
-        # if target node found:
-        #   , kick of subprocess for rest of dfs to see what children are of distance k
-        #    Store the distance of target node from root.
-        #    afterwards of finding said target node:
-        #.        pass root in to find the distance from root (same process as before), but 
-        #        this time with a new distance that incorporates target distance to root.
-        return
+        def findTarget(node: Optional[TreeNode]) -> List[int]:
+            nonlocal target
+            path = []
+            if not node:
+                return
+            elif node == target:
+                return path
+            else:  # Not found.
+                path.append(node)
+                return findTarget(node.left) or findTarget(node.right)
+        target_path = findTarget(root)
+        if target_path:
+            print(target_path)
