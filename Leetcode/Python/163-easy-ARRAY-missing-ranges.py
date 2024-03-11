e31b2b4dc885d81abe6158c2b9903c5d1da6cc01
daviscvance <daviscvance@gmail.com>
2024-03-10 20:07:02 -0400

    2024-03-10

--- Leetcode/Python/163-easy-ARRAY-missing-ranges.py
+++ Leetcode/Python/163-easy-ARRAY-missing-ranges.py
@@ -3,25 +3,25 @@
 # Array
 # https://leetcode.com/problems/missing-ranges
 #
-# Find
+# Find all the gaps in a given range and boundary.
 # def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
+# Input: nums = [0,1,3,50,75], lower = 0, upper = 99
+# Output: [[2,2],[4,49],[51,74],[76,99]]
+
+from itertools import pairwise
+from typing import List
 
 class Solution:
+    # Constant space loop | Time: O(n) | Space: O(1)
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
+        # Return a list of lows and highs for each pairwise if they are bounded correctly.
+        return ([l+1, r-1] for l, r in pairwise([lower-1] + nums + [upper+1]) if l+1 <= r-1)
+
+    # Stored result loop | Time: O(n) | Space: O(n)
     def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
         missing = []
-        nums.append()
-        for (il, left), (ir, right) in enumerate(pairwise(nums)):
-            print(left, right)
-            if lower < left < upper:
-                begin = max(left, lower)
-            if lower < right < upper:
-                end = min(right, upper)
-            if end - begin > 1 :
-                missing.append([begin + 1, end - 1])
+        for n in nums + [upper + 1]:
+            if n > lower:
+                missing.append([lower, n - 1])
+            lower = n + 1
         return missing
-            # elif begin <= end:
-            #     # begin, end = upper, lower
-
-            # if missing:
-            #     print(missing)
-            # print(num - 1)
---
+++ Leetcode/Python/169-easy-ARRAY-majority-element.py
@@ -0,0 +1,39 @@
+# 169. Majority Element
+# Easy
+# Array, Hash Table, Divide and Conquer, Sorting, Counting
+# https://leetcode.com/problems/majority-element
+#
+# Return the most frequent element.
+# def majorityElement(self, nums: List[int]) -> int:
+# Input: nums = [2,2,1,1,1,2,2]
+# Output: 2
+
+from collections import defaultdict, Counter
+from typing import List
+
+class Solution:
+    # Counter w/ early stop | Time: O(n) | Space: O(n)
+    def majorityElement(self, nums: List[int]) -> int:
+        N = defaultdict(int)
+        max_key = len(nums) / 2
+        for key in nums:
+            N[key] += 1
+            if N[key] >= max_key:
+                return key
+
+    # Counter | Time: O(n) | Space: O(n)
+    def majorityElement(self, nums: List[int]) -> int:
+        C = Counter(nums)
+        return max(C.keys(), key=C.get)
+
+    # Boyer-Moore Voting Algorithm | Time: O(n) | Space: O(1)
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        return candidate
