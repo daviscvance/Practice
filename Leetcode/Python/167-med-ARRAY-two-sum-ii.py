403f616d9414526f0eaf798497ba16f916056064
daviscvance <daviscvance@gmail.com>
2024-03-10 23:46:47 -0400

    2024-03-10

--- Leetcode/Python/167-med-ARRAY-two-sum-ii.py
+++ Leetcode/Python/167-med-ARRAY-two-sum-ii.py
@@ -3,18 +3,22 @@
 # Array, Two Pointers, Binary Search
 # https://leetcode.com/problems/two-sum-ii-input-array-is-sorted
 #
-# Given a sorted ascending array, Sum two numbers to a target.
+# Given a sorted ascending array, sum two numbers to a target.
 # Return the indices of the array where index1 < index2.
+# Input: numbers = [2,3,4], target = 6
+# Output: [1,3]
+
+from typing import List
 
 class Solution:
-    # Time: O(n) | Space: O(1)
-    def twoSum(self, numbers: list[int], target: int) -> list[int]:
-        left, right = 0, len(numbers) - 1
-        while left < right:
-            current_sum = numbers[left] + numbers[right]
-            if current_sum == target:
-                return [left + 1, right + 1]
-            elif current_sum < target:
-                left += 1
+    # Two Pointer | Time: O(n) | Space: O(1)
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        lo, hi = 0, len(numbers) - 1
+        while lo < hi:
+            pair_sum = numbers[lo] + numbers[hi]
+            if pair_sum == target:
+                return [lo + 1, hi + 1]
+            elif pair_sum < target:
+                lo += 1
             else:
-                right -= 1
+                hi -= 1
