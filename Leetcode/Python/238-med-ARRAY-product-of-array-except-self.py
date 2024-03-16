6847ec65f5de22dc05729e32cb79cea3887a182e
daviscvance <daviscvance@gmail.com>
2024-03-15 23:10:56 -0400

    2024-11-15

---
+++ Leetcode/Python/238-med-ARRAY-product-of-array-except-self.py
@@ -0,0 +1,27 @@
+# 238. Product of Array Except Self
+# Medium
+# Array, Prefix Sum
+# https://leetcode.com/problems/product-of-array-except-self
+#
+# Calculate the product of all elements in an array except for the given index.
+# def productExceptSelf(self, nums: List[int]) -> List[int]:
+# Input: nums = [1,2,3,4]
+# Output: [24,12,8,6]
+
+class Solution:
+    # Time: O(n) | Space: O(1)
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+        products = [1] * length
+
+        prefixProduct = 1
+        for i in range(length):
+            products[i] = prefixProduct
+            prefixProduct *= nums[i]
+
+        suffixProduct = 1
+        for i in range(length - 1, -1, -1):
+            products[i] *= suffixProduct
+            suffixProduct *= nums[i]
+
+        return products
