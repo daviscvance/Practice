000bb8b77e9e78a8ccf8c5a60b5cbfc86db64ac8
daviscvance <daviscvance@gmail.com>
2024-03-25 23:57:00 -0400

    2024-03-25

--- Leetcode/Python/525-med-ARRAY-contiguous-array.py
+++ Leetcode/Python/525-med-ARRAY-contiguous-array.py
@@ -9,25 +9,15 @@
 # Output: 2
 
 class Solution:
-    # Dynamic Programming | Time: O(n) | Space: O(n)
-    def findMaxLength(self, nums: List[int]) -> int:
-        N = len(nums)
-        qualifying = [False] * N
-        tracker = 0
-        for i, b in enumerate(nums):
-            if b == 0:
-                b = -1
-            tracker += (1 if b else -1)
-            if tracke5r== 0:
-                qualifying[i] = True
-
-        longest = 0
-        tracker = 0        
-        for i, v in enumerate(qualifying):
-            if not v:
-                tracker = 0
+    # Prefix Sum | Time: O(n) | Space: O(n)
+    def findMaxLength(self, nums: list[int]) -> int:
+        prefix = {}
+        count = longest = 0
+        for idx, num in enumerate(nums):
+            count += (1 if num else -1)
+            if not count:
+                longest = idx + 1
             else:
-                tracker += v
-            longest = max(longest, tracker)
-        return longest + 1
-
+                count_last_seen = prefix.setdefault(count, idx)
+                longest = max(longest, idx - count_last_seen)
+        return longest
