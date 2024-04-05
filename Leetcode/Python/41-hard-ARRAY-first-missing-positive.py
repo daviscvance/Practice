0354124d33f5097c59f281a605ff21b385ea7211
daviscvance <daviscvance@gmail.com>
2024-04-05 00:05:12 -0400

    .

--- Leetcode/Python/41-hard-ARRAY-first-missing-positive.py
+++ Leetcode/Python/41-hard-ARRAY-first-missing-positive.py
@@ -1,8 +1,12 @@
 # 41. First Missing Positive
 # Hard
 # Array, Hash Table
+# https://leetcode.com/problems/first-missing-positive
 #
 # Return the smallest missing positive integer from an unsorted array.
+# def firstMissingPositive(self, nums: List[int]) -> int:
+# Input: nums = [7,8,9,11,12]
+# Output: 1
 
 from typing import List
 
@@ -30,3 +34,90 @@ class Solution:
             if num > -1:
                 return idx
         return pos_count + 1
+
+    # Checks | Time: O(n)| Space: O(n)
+    def firstMissingPositive(self, nums):
+        n = len(nums)
+        seen = [False] * (n + 1)  # Array for lookup
+
+        # Mark the elements from nums in the lookup array
+        for num in nums:
+            if 0 < num <= n:
+                seen[num] = True
+
+        # Iterate through integers 1 to n
+        # return smallest missing positive integer
+        for i in range(1, n + 1):
+            if not seen[i]:
+                return i
+
+        # If seen contains all elements 1 to n
+        # the smallest missing positive number is n + 1
+        return n + 1
+
+    # Index as a hash key | Time: O(n) | Space: O(n)
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+        contains_1 = False
+
+        # Replace negative numbers, zeros,
+        # and numbers larger than n with 1s.
+        # After this nums contains only positive numbers.
+        for i in range(n):
+            # Check whether 1 is in the original array
+            if nums[i] == 1:
+                contains_1 = True
+            if nums[i] <= 0 or nums[i] > n:
+                nums[i] = 1
+
+        if not contains_1:
+            return 1
+
+        # Mark whether integers 1 to n are in nums
+        # Use index as a hash key and negative sign as a presence detector.
+        for i in range(n):
+            value = abs(nums[i])
+            # If you meet number a in the array - change the sign of a-th element.
+            # Be careful with duplicates : do it only once.
+            if value == n:
+                nums[0] = -abs(nums[0])
+            else:
+                nums[value] = -abs(nums[value])
+
+        # First positive in nums is smallest missing positive integer
+        for i in range(1, n):
+            if nums[i] > 0:
+                return i
+
+        # nums[0] stores whether n is in nums
+        if nums[0] > 0:
+            return n
+
+        # If nums contained all elements 1 to n
+        # the smallest missing positive number is n + 1
+        return n + 1
+
+    # Cycle sort | Time: O(n) | Space: O(n)
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        # Use cycle sort to place positive elements smaller than n
+        # at the correct index
+        i = 0
+        while i < n:
+            correct_idx = nums[i] - 1
+            if 0 < nums[i] <= n and nums[i] != nums[correct_idx]:
+                # swap
+                nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
+            else:
+                i += 1
+
+        # Iterate through nums
+        # return smallest missing positive integer
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+
+        # If all elements are at the correct index
+        # the smallest missing positive number is n + 1
+        return n + 1
